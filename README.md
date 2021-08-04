a6425ba65d2cd5f07b6637ff31336b9e8937359b
Daniel Eiji Higa <higabyte@gmail.com>
2021-08-04 14:15:40 -0300

    Updating README and android application inference for one image in testset folder.

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # IPE
 ## Document Specification
 [Android ML Project](/docs/Android_ML_Project.pdf)
+
+## Steps
+### 1. Downloading datasets
+https://github.com/myleott/mnist_png (in png format). 30 images have been chosen to embed in Android Application located in assets/testset folder.
+
+### 2. Machine Learning models (tflite output)
+a) Exp01
+https://colab.research.google.com/github/tensorflow/examples/blob/master/lite/codelabs/digit_classifier/ml/step2_train_ml_model.ipynb#scrollTo=Q_Z5yLxrwbpI
+
+b) Exp02
+https://colab.research.google.com/github/tensorflow/examples/blob/master/lite/codelabs/digit_classifier/ml/step7_improve_accuracy.ipynb#scrollTo=IPnP4xPbjqWi
+
+### 3. Evaluation
+* Exp 01 - Initial model
+313/313 [==============================] - 5s 16ms/step - loss: 5.6524 - accuracy: 0.3342
+[5.652446269989014, 0.334199994802475]
+
+* Exp 02 - Improved model
+313/313 [==============================] - 5s 15ms/step - loss: 0.2454 - accuracy: 0.9222
+[0.2453950196504593, 0.9222000241279602]
+
+### 4. Android implementation
+Open Project in Android Studio using:
+https://github.com/higa-daniel/ipe/tree/main/android
+
+### 5. API level requirements
+TBD
--- android/app/src/main/java/org/tensorflow/lite/codelabs/digitclassifier/MainActivity.kt
+++ android/app/src/main/java/org/tensorflow/lite/codelabs/digitclassifier/MainActivity.kt
@@ -14,86 +14,112 @@ limitations under the License.
 package org.tensorflow.lite.codelabs.digitclassifier
 
 import android.annotation.SuppressLint
-import android.graphics.Color
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
-import android.view.MotionEvent
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.divyanshu.draw.widget.DrawView
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
-  private var drawView: DrawView? = null
-  private var clearButton: Button? = null
-  private var predictedTextView: TextView? = null
-  private var digitClassifier = DigitClassifier(this)
+    private var nextButton: Button? = null
+    private var predictedTextView: TextView? = null
+    private var digitClassifier = DigitClassifier(this)
+    private lateinit var imageView: ImageView
+    private var fileName = ""
 
-  @SuppressLint("ClickableViewAccessibility")
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "OnCreate")
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    // Setup view instances.
-    drawView = findViewById(R.id.draw_view)
-    drawView?.setStrokeWidth(70.0f)
-    drawView?.setColor(Color.WHITE)
-    drawView?.setBackgroundColor(Color.BLACK)
-    clearButton = findViewById(R.id.clear_button)
-    predictedTextView = findViewById(R.id.predicted_text)
+        // Initializing ImageView
+        imageView = findViewById<ImageView>(R.id.imageView)
+        imageView.setImageResource(R.mipmap.ic_launcher_round);
 
-    // Setup clear drawing button.
-    clearButton?.setOnClickListener {
-      drawView?.clearCanvas()
-      predictedTextView?.text = getString(R.string.prediction_text_placeholder)
+        // Setup view instances
+        predictedTextView = findViewById(R.id.predicted_text)
+
+        // Setup digit classifier
+        digitClassifier
+            .initialize()
+            .addOnFailureListener { e -> Log.e(TAG, "Error to setting up digit classifier.", e) }
     }
 
-    // Setup classification trigger so that it classify after every stroke drew.
-    drawView?.setOnTouchListener { _, event ->
-      // As we have interrupted DrawView's touch event,
-      // we first need to pass touch events through to the instance for the drawing to show up.
-      drawView?.onTouchEvent(event)
+    override fun onResume() {
+        Log.d(TAG, "OnResume")
+        super.onResume()
 
-      // Then if user finished a touch event, run classification
-      if (event.action == MotionEvent.ACTION_UP) {
-        classifyDrawing()
-      }
+        nextButton = findViewById<Button>(R.id.next_button) as Button
+        nextButton!!.setOnClickListener {
+            // Get an image from assets folder
+            fileName = "img_01.png"
+            val bitmap: Bitmap? = assetsToBitmap(fileName)
 
-      true
-    }
+            // Classify image
+            classifyImage(bitmap)
 
-    // Setup digit classifier.
-    digitClassifier
-      .initialize()
-      .addOnFailureListener { e -> Log.e(TAG, "Error to setting up digit classifier.", e) }
-  }
-
-  override fun onDestroy() {
-    // Sync DigitClassifier instance lifecycle with MainActivity lifecycle,
-    // and free up resources (e.g. TF Lite instance) once the activity is destroyed.
-    digitClassifier.close()
-    super.onDestroy()
-  }
-
-  private fun classifyDrawing() {
-    val bitmap = drawView?.getBitmap()
-
-    if ((bitmap != null) && (digitClassifier.isInitialized)) {
-      digitClassifier
-        .classifyAsync(bitmap)
-        .addOnSuccessListener { resultText -> predictedTextView?.text = resultText }
-        .addOnFailureListener { e ->
-          predictedTextView?.text = getString(
-            R.string.classification_error_message,
-            e.localizedMessage
-          )
-          Log.e(TAG, "Error classifying drawing.", e)
+            // Show bitmap on image view
+            bitmap?.apply {
+                imageView = findViewById<ImageView>(R.id.imageView)
+                imageView.setImageBitmap(this)
+            }
         }
     }
-  }
 
-  companion object {
-    private const val TAG = "MainActivity"
-  }
+    override fun onDestroy() {
+        Log.d(TAG, "OnDestroy")
+        // Sync DigitClassifier instance lifecycle with MainActivity lifecycle, and free up resources (e.g. TF Lite instance) once the activity is destroyed.
+        digitClassifier.close()
+        super.onDestroy()
+    }
+
+    // Auxiliary functions
+    // extension function to set image view image from assets folder
+    fun ImageView.setImageAssets(context: Context, fileName: String) {
+        try {
+            with(context.assets.open(fileName)) {
+                setImageBitmap(BitmapFactory.decodeStream(this))
+            }
+        } catch (e: IOException) {
+            // log error
+        }
+    }
+
+    // extension function to get bitmap from assets
+    fun Context.assetsToBitmap(fileName: String): Bitmap? {
+        return try {
+            with(assets.open(fileName)) {
+                BitmapFactory.decodeStream(this)
+            }
+        } catch (e: IOException) {
+            null
+        }
+    }
+
+    private fun classifyImage(bitmap: Bitmap?) {
+        Log.d(TAG, "classifyImage")
+        if ((bitmap != null) && (digitClassifier.isInitialized)) {
+            digitClassifier
+                .classifyAsync(bitmap)
+                .addOnSuccessListener { resultText -> predictedTextView?.text = resultText }
+                .addOnFailureListener { e ->
+                    predictedTextView?.text = getString(
+                        R.string.classification_error_message,
+                        e.localizedMessage
+                    )
+                    Log.e(TAG, "Error classifying drawing.", e)
+                }
+        }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
 }
